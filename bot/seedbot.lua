5dd629614d68dd48ba4dd5e2c8f4613d6c73ef74
peiman08 <coinline953@gmail.com>
2016-04-26 08:14:03 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -515,3 +515,14 @@ our_id = 0
 now = os.time()
 math.randomseed(now)
 started = false
+# Install dependencies.
+# Tested on Ubuntu 14.04. For other OSs, check out https://github.com/yagop/telegram-bot/wiki/Installation
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+
+# Let's install the bot.
+cd $HOME
+git clone https://github.com/SEEDTEAM/TeleSeed.git
+cd TeleSeed
+chmod +x launch.sh
+./launch.sh install
+./launch.sh # Enter a 09332336308& confirmation code.
